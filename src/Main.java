4bb351f7987e881be83cbd40d35e7b76db5dd623
Your <you>
2023-01-27 17:47:22 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 89366; // Стоимость билета
+        int amount = 20; // Количество рублей для одной бонусной мили
+
+        System.out.println(" При покупке авиабилета, Вы получаете бонусные мили. ");
+        System.out.println((ticket / amount)+ " бонусных миль ");
+
+
+    }
+}
